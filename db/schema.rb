3c0270cbe2d7867ae465e31b6283d2894a7c2284
John Tweeddale <j.tweeddale@outlook.com>
2021-05-24 15:20:21 +0100

    lists routing, controller actions and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_163418) do
+ActiveRecord::Schema.define(version: 2021_05_24_111402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_05_21_163418) do
     t.string "poster_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "rating"
   end
 
   add_foreign_key "bookmarks", "lists"
