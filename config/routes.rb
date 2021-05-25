a1cf9ef0e9d4a7ee77addbad42b01257e2004547
John Tweeddale <j.tweeddale@outlook.com>
2021-05-25 10:20:38 +0100

    added lists new and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get "/lists/new", to: "lists#new"
-  get "lists", to: "lists#index"
-  get "/lists/:id", to: "lists#show"
-  post "/lists", to: "lists#create"
+  resources :lists do
+    get "/bookmarks/new", to: "bookmarks#new"
+  end
 end
