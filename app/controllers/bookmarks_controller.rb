dd1d5234d06913a446a613a2f015e171eff4fd49
John Tweeddale <j.tweeddale@outlook.com>
2021-05-25 11:44:33 +0100

    added new and create actions for bookmark for a user's list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,21 @@
 class BookmarksController < ApplicationController
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save!
+      redirect_to list_path(@list)
+    else
+      render "create"
+    end
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :lists do
-    get "/bookmarks/new", to: "bookmarks#new"
+    # get "/bookmarks/new", to: "bookmarks#new"
+    resources :bookmarks, only: [:new, :create]
   end
 end
